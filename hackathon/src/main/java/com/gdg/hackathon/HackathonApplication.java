a1cd5996bfa3a8e88080dd6b82ab3bddec426c50
young-in <hardworkingini@gmail.com>
2024-10-15 00:21:34 +0900

    feat : application-secret 인식을 위한 코드 수정

--- hackathon/src/main/java/com/gdg/hackathon/HackathonApplication.java
+++ hackathon/src/main/java/com/gdg/hackathon/HackathonApplication.java
@@ -1,13 +1,16 @@
 package com.gdg.hackathon;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class HackathonApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(HackathonApplication.class, args);
+	public static void main(String[] args){
+		new SpringApplicationBuilder(HackathonApplication.class)
+				.properties("spring.config.location=classpath:/application.yml,classpath:/application-secret.yml")
+				.run(args);
 	}
 
 }
